3e224b04fe7c6480d9947cd1e11889790935735a
Valrandir <valrandir@voxcel.net>
2014-01-14 20:41:51 -0500

    First Run

--- OnlyOne.cpp
+++ OnlyOne.cpp
@@ -16,16 +16,8 @@ void fail()
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
-	IDirectSound8* lpDirectSound = NULL;
 	HRESULT hResult;
 	BOOL result;
-
-	//Init DirectSound
-	hResult = DirectSoundCreate8(NULL, &lpDirectSound, NULL);
-	if(hResult != DS_OK) fail();
-
-	hResult = lpDirectSound->SetCooperativeLevel(GetDesktopWindow(), DSSCL_NORMAL);
-	if(hResult != DS_OK) fail();
 
 	//Load Wave File
 	struct RuffWaveType
@@ -47,6 +39,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 	result = ReadFile(hFile, &ruff_wave, bytes_to_read, &bytes_read, NULL);
 	if(!result && bytes_read != bytes_to_read) fail();
 
+	if(ruff_wave.chunk_id != 0x52494646 /*RIFF*/) fail();
+	if(ruff_wave.format != 0x57415645 /*WAVE*/) fail();
+	if(ruff_wave.chunk_fmt_id != 0x666d7420 /*fmt*/) fail();
+
 	ruff_wave.data = malloc(ruff_wave.chunk_data_size);
 	if(ruff_wave.data == 0) fail();
 
@@ -57,6 +53,51 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 	CloseHandle(hFile);
 	hFile = NULL;
 
+	WAVEFORMATEX wave_format = {0};
+	wave_format.wFormatTag = ruff_wave.audio_format;
+	wave_format.nChannels = ruff_wave.num_channel;
+	wave_format.nSamplesPerSec = ruff_wave.sample_rate;
+	wave_format.nBlockAlign = ruff_wave.block_align;
+	wave_format.nAvgBytesPerSec = wave_format.nBlockAlign * wave_format.nSamplesPerSec;
+
+	//Init DirectSound
+	IDirectSound8* lpDirectSound = NULL;
+	hResult = DirectSoundCreate8(NULL, &lpDirectSound, NULL);
+	if(hResult != DS_OK) fail();
+
+	hResult = lpDirectSound->SetCooperativeLevel(GetDesktopWindow(), DSSCL_NORMAL);
+	if(hResult != DS_OK) fail();
+
+	DSBUFFERDESC ds_buffer_desc = {0};
+	ds_buffer_desc.dwSize = sizeof(DSBUFFERDESC);
+	ds_buffer_desc.dwFlags = DSBCAPS_STATIC;
+	ds_buffer_desc.dwBufferBytes = ruff_wave.chunk_data_size;
+	ds_buffer_desc.lpwfxFormat = &wave_format;
+
+	IDirectSoundBuffer* lpDirectSoundBuffer;
+	hResult = lpDirectSound->CreateSoundBuffer(&ds_buffer_desc, &lpDirectSoundBuffer, NULL);
+	if(hResult != DS_OK) fail();
+
+	//Load Sound Data into DirectSoundBuffer
+	void* locked_buffer;
+	DWORD locked_buffer_size;
+
+	hResult = lpDirectSoundBuffer->Lock(0, 0, &locked_buffer, &locked_buffer_size, 0, 0, DSBLOCK_ENTIREBUFFER);
+	if(hResult != DS_OK) fail();
+	memcpy(locked_buffer, ruff_wave.data, locked_buffer_size);
+	hResult = lpDirectSoundBuffer->Unlock(locked_buffer, locked_buffer_size, 0, 0);
+	if(hResult != DS_OK) fail();
+
+	//Play
+	hResult = lpDirectSoundBuffer->Play(NULL, 0, 0);
+	if(hResult != DS_OK) fail();
+
+	//Wait
+	Sleep(12000);
+
+	//Free
+	lpDirectSoundBuffer->Release();
+	lpDirectSound->Release();
 	free(ruff_wave.data);
 	ruff_wave.data = NULL;
 
